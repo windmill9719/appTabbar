3ce7f297b9ea2656e82a563feec532f2d23e640f
ErinLiu <1125489858@qq.com>
2019-11-01 18:12:42 +0800

    alias config

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,10 @@
-module.exports = {
-  configureWebpack:{
-    resolve:{
-      alias:{
-        '@': 'src',
-        'assets': '@/assets',
-        'common': '@/common',
-        'components': '@/components',
-        'network': '@/network',
-        'views': '@/views'
-      }
-    }
-  }
-}
+// module.exports =  {
+//   configureWebpack: {
+//     resolve:{
+//       alias:{
+//         // '@': 'src',
+//         'components': 'src/components'
+//       }
+//     }
+//   }
+// }
