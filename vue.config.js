4ab84a44c4917ca851202970064626ca5a35dd4e
ErinLiu <1125489858@qq.com>
2019-11-01 18:24:00 +0800

    resolve alias problem

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
-// module.exports =  {
-//   configureWebpack: {
-//     resolve:{
-//       alias:{
-//         // '@': 'src',
-//         'components': 'src/components'
-//       }
-//     }
-//   }
-// }
+const path = require('path');
+
+module.exports = {
+  chainWebpack: config => {
+    //this path is specific to my project
+    config.resolve.alias.set('assets', path.resolve('src/assets'));
+    config.resolve.alias.set('components', path.resolve('src/components'));
+    config.resolve.alias.set('common', path.resolve('src/common'));
+    config.resolve.alias.set('views', path.resolve('src/views'));
+  },
+};
